77bbf7e027bff071a7367ff11637d80fbbbdf2c3
Shirdartho Bhowmick Tirtho <67155469+Tirtho304@users.noreply.github.com>
2022-02-21 13:10:12 +0600

    1st commit

---
+++ Dart/bin/Prac1.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  final cityName = stdin.readLineSync();
+  print(cityName);
+  //cityName = "INdia";
+  const pi = 3.1416;
+  print(pi);
+  youo.y3;
+  print(youo.y3);
+  // youo.y2;
+}
+
+class youo {
+  final y2 = "Hakimi";
+  //ei kahne shudu const use kra jabe nah static const use krte hbe
+  static const y3 = "Messi";
+}
